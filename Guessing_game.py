7d5943446d52cf2df8093a04037285131b2a029a
ShefChefPastry <matthew.sheffield62@gmail.com>
2026-07-18 15:16:13 -0500

    This is the first version of the game.

---
+++ Guessing_game.py
@@ -0,0 +1,27 @@
+#ok my first game in python 7/17/2026 @7:45PM
+
+
+# program display
+print("===============")
+print("Number Guessing Game") 
+print("===============")
+input("PRESS ENTER TO START")
+
+
+# character selection
+name = input("Enter Player Name: ")
+print("Welcome to my first game in Python,",name,"!")
+
+
+# random number generator 
+import random
+secret_number = random.randint(1, 100)
+guess = int(input("Enter a number between 1-100: "))
+while guess != secret_number:
+    if guess > secret_number:
+        print("Too High! Try again")
+    else:
+        print("Too Low! Try again")
+    guess = int(input("Enter a number: "))
+  
+print("Congratulations! You Win!")
