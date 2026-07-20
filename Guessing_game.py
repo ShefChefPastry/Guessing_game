473c741231e037aafb7960904223a6aaf4407d91
ShefChefPastry <matthew.sheffield62@gmail.com>
2026-07-19 20:25:55 -0500

    Fixes guesses that are too high or too low for that current difficulty

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,4 +1,5 @@
 #ok my first game in python 7/17/2026 @7:45PM
+import random
 
 
 # program display
@@ -10,18 +11,65 @@ input("PRESS ENTER TO START")
 
 # character selection
 name = input("Enter Player Name: ")
-print("Welcome to my first game in Python,",name,"!")
+print(f"Welcome to my first game in Python, {name}!")
+
+
+# choose your difficulty rating
+difficulty_rating = input("Choose difficulty (Easy, Medium, or Hard): ")
+
+while(
+    difficulty_rating != "Easy"
+    and difficulty_rating != "Medium"
+    and difficulty_rating != "Hard"
+):
+     print("Invalid difficulty.") 
+     difficulty_rating = input("Please enter Easy, Medium, or Hard: ")
+
+if difficulty_rating == "Easy":
+    max_number = 100
+    secret_number = random.randint(1, max_number)
+elif difficulty_rating == "Medium":
+    max_number = 500
+    secret_number = random.randint(1, max_number)
+elif difficulty_rating == "Hard":
+    max_number = 1000
+    secret_number = random.randint(1, max_number)
+
 
 
 # random number generator 
-import random
-secret_number = random.randint(1, 100)
-guess = int(input("Enter a number between 1-100: "))
+attempts_made = 0
+guess = int(input(f"Enter a number between 1 and {max_number}: "))
+while (
+        guess < 1
+        or guess > max_number
+):
+    print(f"Invalid number! Please choose a number between 1 and {max_number}.")
+    guess = int(input(f"Enter a number between 1 and {max_number}: "))    
+
+attempts_made += 1
+
 while guess != secret_number:
     if guess > secret_number:
         print("Too High! Try again")
     else:
         print("Too Low! Try again")
-    guess = int(input("Enter a number: "))
+    guess = int(input(f"Enter a number between 1 and {max_number}: "))
+    while (
+    guess < 1
+    or guess > max_number
+):
+        print(f"Invalid number! Please choose a number between 1 and {max_number}.")
+        guess = int(input(f"Enter a number between 1 and {max_number}: "))
+    
+    attempts_made += 1
   
 print("Congratulations! You Win!")
+
+if attempts_made == 1:  
+    print("It took you 1 attempt.") 
+else: 
+    print("It took you", attempts_made, "attempts.") 
+
+
+
