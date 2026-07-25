52f4a30a1f56b1bbea0ed150ec205fff296fa651
ShefChefPastry <matthew.sheffield62@gmail.com>
2026-07-24 23:06:51 -0500

    Add attempt limits and game statistics

--- Guessing_game.py
+++ Guessing_game.py
@@ -45,18 +45,23 @@ def play_game():
 
     if difficulty_rating == "Easy":
         max_number = 100
-        secret_number = random.randint(1, max_number)
+        max_attempts = 10
+
     elif difficulty_rating == "Medium":
         max_number = 500
-        secret_number = random.randint(1, max_number)
-    elif difficulty_rating == "Hard":
+        max_attempts = 10
+
+    else:
         max_number = 1000
-        secret_number = random.randint(1, max_number)
+        max_attempts = 10
+
+    secret_number = random.randint(1, max_number)
 
 
 
     #random number generator
     attempts_made = 0
+    won = False
 
     #first guess
     guess = get_guess(max_number)
@@ -64,12 +69,15 @@ def play_game():
     attempts_made += 1
 
     #Main guessing loop
-    while guess != secret_number:
+    while guess != secret_number and attempts_made < max_attempts:
 
         if guess > secret_number:
             print("Too High! Try again")
         else:
             print("Too Low! Try again")
+
+        attempts_remaining = max_attempts - attempts_made
+        print ("Attempts Remaining:", attempts_remaining)
 
         #Get next guess
         guess = get_guess(max_number)
@@ -78,15 +86,48 @@ def play_game():
         attempts_made += 1
 
 
-    print("Congratulations! You Win!")
+    if guess == secret_number:
+        won = True
+        print("Congratulations! You Win!")
+
+    else:
+        print("You Lost! Too many guesses.")
+        print(f"The secret number was {secret_number}.")
 
     if attempts_made == 1:  
         print("It took you 1 attempt.") 
     else: 
-        print("It took you", attempts_made, "attempts.") 
+        print("You used all", attempts_made, "attempts.") 
+
+    return attempts_made, won
+
+
+#games played and attempts made counter
+games_played = 0
+total_attempts = 0
+best_score = None
 
 while True:
-    play_game()
+    attempts, won = play_game()
+    games_played += 1
+    total_attempts += attempts
+    average_attempts = total_attempts / games_played
+
+
+    if won:
+        if best_score is None:
+            best_score = attempts
+
+    elif attempts < best_score:
+        best_score = attempts
+
+    print("========== Statistics ==========")
+    print("Games Played:", games_played)
+    print("Attempts This Game:", attempts)
+    print("Total Attempts:", total_attempts)
+    print("Average Attempts:", average_attempts)
+    print("================================")
+    print("Your Best Score is:", best_score)
 
     restart = input("Would you like to play again? (y/n): ")
 
